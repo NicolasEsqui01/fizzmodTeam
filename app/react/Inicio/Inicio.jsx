ff4f26494463ace89c841403ff18b67e0fcba85a
sebasdominguez <sebastian.d83@gmail.com>
2020-07-14 18:13:48 -0300

    terminado Lat Izq

--- app/react/Inicio/Inicio.jsx
+++ app/react/Inicio/Inicio.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-elastic-carousel';
 import {
   Container,
   InicioA,
+  InicioB,
   LDerecho,
   Oval,
   Indicadores,
@@ -11,11 +12,28 @@ import {
   Qty,
   Orders,
   Icos24,
+  Icos24Hor,
   PNames,
   PNumbers,
   Logos,
+  LogoStad,
+  LogoUser,
+  ContUser,
+  SubTitle,
+  Item,
+  Time,
+  Percentage,
+  EstadTiempo,
+  Line,
+  ContNumGreen,
+  ContNumRed
 } from './style';
 import boxGreen from '../../images/boxGreen.svg';
+import down from '../../images/chevron_downWhite.svg';
+import up from '../../images/chevron_upWhite.svg';
+import clockWhite from '../../images/clockWhite.svg';
+import squareWhite from '../../images/square_chart_line.svg';
+import user from '../../images/user_closed.svg';
 import clockRed from '../../images/clock-red.svg';
 import triangleWhite from '../../images/triangle_circle_white.svg';
 import '../common/styles/main.scss';
@@ -40,48 +58,45 @@ export default () => (
         </Oval>
         <Indicadores>
           <Icos24>
-            <Logos src={boxGreen} />
+            <Logos src={boxGreen} alt="box"/>
             <PNumbers>7</PNumbers>
             <PNames>X PICKER</PNames>
           </Icos24>
           <Icos24>
-            <Logos src={clockRed} />
+            <Logos src={clockRed} alt="clock"/>
             <PNumbers>7</PNumbers>
             <PNames>X ORDEN</PNames>
           </Icos24>
           <Icos24>
-            <Logos src={triangleWhite} />
+            <Logos src={triangleWhite} alt="triangle"/>
             <PNumbers>7</PNumbers>
             <PNames>X ORDEN</PNames>
           </Icos24>
         </Indicadores>
       </InicioA>
-      <InicioA>
-        <Oval>
-          <OvalInt>
-            <Pickers>4 PICKERS</Pickers>
-            <Qty>28</Qty>
-            <Orders>ORDENES</Orders>
-          </OvalInt>
-        </Oval>
-        <Indicadores>
-          <Icos24>
-            <Logos src={boxGreen} />
-            <PNumbers>7</PNumbers>
-            <PNames>X PICKER</PNames>
-          </Icos24>
-          <Icos24>
-            <Logos src={clockRed} />
-            <PNumbers>7</PNumbers>
-            <PNames>X ORDEN</PNames>
-          </Icos24>
-          <Icos24>
-            <Logos src={triangleWhite} />
-            <PNumbers>7</PNumbers>
-            <PNames>X ORDEN</PNames>
-          </Icos24>
-        </Indicadores>
-      </InicioA>
+      <InicioB>
+        <SubTitle>MIS ESTADÍSTICAS</SubTitle>
+        <ContUser>
+          <LogoUser src={user} alt="user"/>
+        </ContUser>
+        <EstadTiempo>
+          <Icos24Hor>
+            <Logos src={clockWhite} alt="clock" />
+            <Item>TIEMPO POR PRODUCTO</Item>
+            <Time>"1,6</Time>
+            <ContNumGreen><Percentage>7%</Percentage><LogoStad src={down} alt="clock" /></ContNumGreen>
+          </Icos24Hor>
+        </EstadTiempo>
+        <Line></Line>
+        <EstadTiempo>
+          <Icos24Hor>
+            <Logos src={squareWhite} alt="clock" />
+            <Item>TIEMPO POR ORDEN</Item>
+            <Time>"1,6</Time>
+            <ContNumRed><Percentage>7%</Percentage><LogoStad src={up} alt="clock" /></ContNumRed>
+          </Icos24Hor>
+        </EstadTiempo>
+      </InicioB>
     </Carousel>
     <LDerecho>Derecho</LDerecho>
   </Container>
