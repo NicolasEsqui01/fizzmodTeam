597d92f3be1cac754aeddc2b706613bc657ae8b5
Nicolas esquivel <nicl_012@icloud.com>
2020-07-15 15:35:25 -0300

    axios

--- app/action/login.js
+++ app/action/login.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 
-const Login = (userLoger) => {
+const Login = (auth) => {
     return {
         type: "USER_LOGING",
-        userLoger,
+        auth,
     }
 }
 
+const headers = {
+    headers:{
+        'Content-Type': 'application/json',
+        'janis-api-key' : 'Bearer',
+        'janis-api-secret' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMGRiZTg4NmNkYTk2OGVkYzMwMDgxOCIsImNsaWVudENvZGUiOiJwbGF0YWZvcm1hNS0yMDIwIiwiaXNEZXYiOmZhbHNlLCJleHAiOjE1OTUwMDEwOTYsImlhdCI6MTU5NDgyODI5Nn0.iMIiWM0axLUrZbnhcrbnbHhX_lBLO-gocavequcZw7Y',
+    }
+}
+
+
 export const setLogin = (obj) => {
+    console.log('entrando por una vex')
     return dispatch => {
-        return axios.post("https://id.janisdev.in/api/login", obj)
-            .then((user) => dispatch(Login(user)))
+        console.log('hola')
+        return axios.post("https://id.janis.in/api/login", obj , headers)
+            .then((token) => dispatch(Login(token)))
     }
 } 
--- app/react/login/LoginContainer.jsx
+++ app/react/login/LoginContainer.jsx
@@ -1,44 +1,43 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import Login from './Login'
-import { setLogin } from "../../action/login"
+import Login from './Login';
+import { setLogin as funcionDeLogin } from '../../action/login';
 
-const LoginContainer = () => {
+const LoginContainer = ({setLogin}) => {
+  const [input, setInput] = useState({});
 
-    const [input, setInput] = useState({});
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(setLogin, "ESTE ES SEEEETLOGIN")
-        const data = {
-            email: event.target[0].value,
-            password: event.target[1].value
-        }
-        setLogin(data);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      email: event.target[0].value,
+      password: event.target[1].value,
     };
+    setLogin(data);
+  };
 
-    const handleChange = (event) => {
-        setInput({ [event.target.name]: event.target.value })
-    };
+  const handleChange = (event) => {
+    setInput({ [event.target.name]: event.target.value });
+  };
 
-    return (
-        <Login handleSubmit={handleSubmit} handleChange={handleChange} valor={input} />
-    )
+  return (
+    <Login
+      handleSubmit={handleSubmit}
+      handleChange={handleChange}
+      valor={input}
+    />
+  );
 };
 
-
 const mapStateToProps = (state) => {
-    console.log(state, "ESTADOOOO")
-    return {
-
-    };
+  return {
+    user: state.loginReducer.user,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        setLogin: (obj) => dispatch(setLogin(obj))
-    };
+  return {
+    setLogin: (obj) => dispatch(funcionDeLogin(obj)),
+  };
 };
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
