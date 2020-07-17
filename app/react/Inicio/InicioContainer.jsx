a7bc7028658bf4589c21b124afcd97979e7ddc37
sebasdominguez <sebastian.d83@gmail.com>
2020-07-17 12:27:10 -0300

    finall

--- app/react/Inicio/InicioContainer.jsx
+++ app/react/Inicio/InicioContainer.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Inicio from './Inicio';
 import { fetchSessions, DamePermiso ,fetchPickers, ChangePending, ChangePicked } from "../../action/inicio";
 
-	t mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => {
 	
   return {
     sessionId: state.loginReducer.user,  //VERIFICAR NOMBRE DEL ESTADO
