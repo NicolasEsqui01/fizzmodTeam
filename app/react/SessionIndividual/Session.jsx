80886f123a3c85499418432c0885a7a8dacd0c3e
vogui <guillevogemedialdea@gmail.com>
2020-07-21 19:31:54 -0300

    Lado derecho session ind

--- app/react/SessionIndividual/Session.jsx
+++ app/react/SessionIndividual/Session.jsx
@@ -2,12 +2,39 @@ import React from 'react'
 import {
 Container,
 LIzquierdo,
-LDerecho
+LDerecho,
+HeaderDiv,
+Text,
+DivScroll,
+Productos,
+Img,
+Icono,
+Icono2,
+Icono3,
+Icono4,
+DivR,
+DivP,
+DivX,
+DivZ,
+DivB,
+Descripcion,
+Num,
+Num2,
+Ean,
+Cantidad,
 } from './style'
 import Navbar from '../Navbar/NavbarContainer';
+import basket from '../../images/basket.svg';
+import scanner from '../../images/scanner.svg';
+import box from '../../images/box.svg';
+import substitute from '../../images/substitute.svg';
+import waves from '../../images/wavesCelest.svg';
+import balance from '../../images/balance.svg';
+import snow from '../../images/snowflakeCelest.svg';
 
 
 export default ()=>{
+    const prueba = [1,2,3,4,5,6,7,8,9,10]
 
 
 return(
@@ -18,14 +45,56 @@ return(
 Lado izquierdo
 
     </LIzquierdo>
+
+
+
     <LDerecho>
-lado derecho
+
+  <HeaderDiv>
+<Text>PRODUCTOS</Text>
+<Text>CANTIDAD</Text>
+   </HeaderDiv>
+
+<DivScroll>
+
+{ prueba.map(elem =>
+<Productos>
+
+   
+    <DivR >
+      <Img src={basket}/>
+    <DivX>
+      <Descripcion>DESCRIPCION DEL PRODUCTO</Descripcion>
+
+    <DivZ>
+      <Icono2 src= {scanner}/>
+      <Ean>1234456112</Ean>
+      <Cantidad>30 / 30</Cantidad>
+    </DivZ>
+
+    </DivX>
+
+    </DivR>
+       <hr></hr>
+    <DivP>
+       <Icono src={basket}/>
+       <Num> 1. </Num>
+       <Icono3 src={box}/>
+       <Num2>5f0507d88459ab000839154e</Num2>
+       <DivB>
+       <Icono4 src={balance}/>
+       <Icono4 src={substitute}/>
+       </DivB>
+    </DivP>
+    
+</Productos>
+)}
+
+</DivScroll>
 
     </LDerecho>
 
-
-
-    </Container>
+</Container>
 
     </>
 
--- app/react/SessionIndividual/style.js
+++ app/react/SessionIndividual/style.js
@@ -22,21 +22,268 @@ export const Container = styled.div`
 export const LIzquierdo = styled.div`
   display: flex;
   flex-direction: column;
-  width: 35%;
+  width: 40%;
   @media only screen and (max-width : 768px) {
   	width: 100%;
     height: 95vh;
   }
 `
+
+
+// DERECHO
+
 export const LDerecho = styled.div`
 
 background-color:#E8EAF6;
-width: 65%;
+width: 60%;
 display: flex;
 flex-direction: column;
+align-items:center;
 @media ${device.tablet} {
     width: 100%;
      display: flex;
     flex-direction: column;
   }
   `;
+
+export const HeaderDiv = styled.div `
+@media ${device.tablet} {
+height:50px;
+}
+display:flex;
+justify-content: space-between;
+width: 100%;
+height: 8%;
+
+`
+
+export const Text = styled.h1 `
+@media ${device.tablet} {
+  font-size: 15px;
+  }
+margin-top: 4%;
+height: 15px;
+  width:30%;
+  color: #273947;
+  font-family: Roboto;
+  font-size: 20px;
+  letter-spacing: 0;
+  line-height: 15px;
+  text-align: center;
+`
+
+export const Productos = styled.div `
+@media ${device.tablet} {
+  height:400px;
+  }
+margin-top:10px;
+background-color:#FFFFFF;
+height:33%;
+display: flex;
+flex-direction: column;
+border-right: 15px solid #2979FF;
+border-top-right-radius: 5px;
+border-bottom-right-radius:5px;
+`
+// omitido #F13B70; picked #08C4C4;
+
+export const DivScroll = styled.div`
+@media ${device.tablet} {
+  
+  height: 90%;
+ 
+}
+  overflow: auto;
+  height: 100%;
+  width: 85%;
+
+`
+
+
+//imgs
+
+export const Img = styled.img `
+@media ${device.tablet} {
+  height:30%;
+  width:20%;
+  }
+height: 60%;
+width: 15%;
+margin-top:3%;
+margin-left:2%;
+`
+
+export const Icono = styled.img `
+ height:50%;
+ width:10%;
+ margin-left:2%;
+ border-left: 3px solid #08C4C4
+`
+export const Icono2 = styled.img `
+@media ${device.tablet} {
+  height:40px;
+  width:50px;
+  }
+ height:30px;
+ width:30px;
+ margin-left:2%;
+ margin-top 10%;
+
+`
+export const Icono3 = styled.img `
+@media ${device.tablet} {
+  height:40%;
+  width:9%;
+  margin-left:0;
+  margin-right:2%;
+  }
+ height:40%;
+ width:7%;
+ margin-left:2%;
+`
+export const Icono4 = styled.img `
+ height:40px;
+ width:40px;
+ margin-left:5%;
+ margin-right:5%;
+`
+
+
+
+export const Descripcion = styled.div `
+@media ${device.tablet} {
+  height:20%;
+  width:90%;
+  }
+width:90%;
+margin-top:3%;
+margin-left:2%;
+height: 40px;
+
+color: #939598;
+font-family: Roboto;
+font-size: 100%;
+letter-spacing: 0;
+line-height: 18px;
+
+`
+export const Ean = styled.div`
+@media ${device.tablet} {
+  height:20%;
+  font-size: 20px;
+  width:70%;
+
+  }
+width:30%;
+color: #273947;
+font-family: Roboto;
+font-size: 20px;
+font-weight: bold;
+letter-spacing: 0;
+margin-left:2%;
+margin-top 10%;
+`
+
+export const Num = styled.p`
+@media ${device.tablet} {
+  width:15%;
+  }
+  height: 30px;
+  width:5%;
+  color: #273947;
+  font-family: Roboto;
+  font-size: 25px;
+  font-weight: 900;
+  letter-spacing: 0;
+`
+export const Num2 = styled.p`
+@media ${device.tablet} {
+  
+  font-size: 15px;
+  }
+height: 16px;
+width: 50%;
+color: #939598;
+font-family: Roboto;
+font-size: 20px;
+letter-spacing: 0;
+line-height: 14px;
+
+`
+
+export const Cantidad =styled.div `
+@media ${device.tablet} {
+  width:80%;
+  margin-left:0;
+  margin-right:0;
+  }
+
+border: 1px solid #E8EAF6;
+margin-left:40%;
+margin-right:5%;
+margin-top 7%;
+font-size:140%;
+color: #273947;
+font-family: Roboto;
+line-height:45px;
+padding:5px;
+height:60%;
+width:30%;
+display:flex;
+justify-content:center;
+align-content: center;
+border-radius:30px
+
+`
+
+
+//controladores
+
+export const DivX = styled.div `
+width:100%;
+`
+
+export const DivR = styled.div `
+@media ${device.tablet} {
+  height:70%;
+  width:95%;
+  }
+height:70%;
+width:100%;
+display:flex;
+
+`
+export const DivP = styled.div `
+@media ${device.tablet} {
+  height:30%;
+  }
+width:100%;
+display:flex;
+height:30%;
+align-items:center;
+
+`
+export const DivZ = styled.div `
+@media ${device.tablet} {
+  display:flex;
+  flex-direction:column;
+  margin-right:20%;
+  }
+display:flex;
+align-items:center;
+`
+export const DivB = styled.div `
+@media ${device.tablet} {
+  display:flex;
+  flex-direction:column;
+  margin-left:10%;
+  }
+
+margin-left: 20%;
+margin-right:5%;
+width:10%;
+height:100%;
+display:flex;
+align-items:center;
+justify-content:center;
+`
