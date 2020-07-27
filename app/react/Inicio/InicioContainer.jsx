23a7ef80ab2ee674781e69649f0facc21f5ffb0f
Nicolas esquivel <nicl_012@icloud.com>
2020-07-27 12:24:40 -0300

    agrege un console.log a la demo

--- app/react/Inicio/InicioContainer.jsx
+++ app/react/Inicio/InicioContainer.jsx
@@ -15,6 +15,7 @@ import history from '../../utils/history'
 import Navbar from '../Navbar/NavbarContainer';
 
 const mapStateToProps = (state, ownProps) => {
+  console.log(state)
   return {
     sessionId: state.sessionReducer.sessionId, // Me trae el id de la session
     session: state.inicioReducer.sessions,
